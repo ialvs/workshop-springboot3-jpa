e30d835026094162baf0fc9d467f0e8766e186bb
Ian Alves <ian.santos.alves@hotmail.com>
2022-11-14 16:46:14 -0300

    Delete User

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -43,7 +43,7 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
-	@DeleteMapping
+	@DeleteMapping(value = "{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
